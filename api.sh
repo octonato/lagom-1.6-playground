66f4e37065518ec96a161f36db6086708c84cfc3
Renato Cavalcanti <renato@cavalcanti.be>
2019-06-05 23:12:17 +0200

    added more functions

--- api.sh
+++ api.sh
@@ -3,6 +3,18 @@
 shopping.batch() {
   for i in {1..40}
   do
-    http POST localhost:$1/shoppingcart/abc-${i}  productId=$2 quantity:=${i} -v
+    http POST localhost:$1/shoppingcart/$2-${i}  productId=$2 quantity:=${i} -v
   done
+}
+
+shopping.report() {
+  http POST localhost:$1/shoppingcart/$2/checkout
+}
+
+shopping.get() {
+  http localhost:$1/shoppingcart/$2
+}
+
+shopping.report() {
+  http localhost:$1/shoppingcart/$2/report
 }
